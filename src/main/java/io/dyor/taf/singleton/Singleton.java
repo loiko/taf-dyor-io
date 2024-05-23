283c69399f7b06e0e767734ec7eac068277af12e
Nikolai Loiko <mikalai.loika@gmail.com>
2024-05-23 23:47:59 +0300

    singleton fixed

--- src/main/java/io/dyor/taf/singleton/Singleton.java
+++ src/main/java/io/dyor/taf/singleton/Singleton.java
@@ -15,7 +15,7 @@ import java.awt.*;
         public static Browser getBrowser() {
             if (browser == null) {
                 playwright = Playwright.create();
-                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                browser = playwright.chromium().launch();
 
                 Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
                 int width = (int) screenSize.getWidth();
